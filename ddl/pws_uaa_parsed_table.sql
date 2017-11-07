78612550d82b234f1d1edb207e3913df71aadd0c
Constantinos (Costas) Antzakas <constantinos.antzakas@gmail.com>
2017-11-07 14:36:13 +0000

    Create pws_uaa_parsed TABLE

---
+++ ddl/pws_uaa_parsed_table.sql
@@ -0,0 +1,23 @@
+CREATE TABLE pws.pws_uaa_parsed
+WITH (appendonly=true, compresstype=zlib, orientation=column, compresslevel=5) 
+AS SELECT logId,
+	trunc(syslogPriority/8) AS syslogFacility,
+	syslogPriority % 8 AS syslogSeverity,
+	syslogEventTime,
+	syslogHostIp,
+	syslogComponentString,
+	syslogJobString,
+	messageTime,
+	logMessagePrefix,
+	logLevel,
+	uaaLogType,
+	uaaEvent,
+	uaaEntitlements,
+	uaaPrincipal,
+	uaaOrigin,
+	uaaIdentityZone,
+	logMessageCatchAll
+FROM (
+	SELECT (pws.plpy_parse_uaa(rawrecord)).* 
+	FROM pws.pws_uaa) A
+DISTRIBUTED BY (logId) ;
