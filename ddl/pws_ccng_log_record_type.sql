7690ffdbd23f73e6f37b1b3e2307c945ad449cc2
Constantinos (Costas) Antzakas <constantinos.antzakas@gmail.com>
2017-11-13 16:34:46 +0000

    Create pws.ccng_syslog_record TYPE

---
+++ ddl/pws_ccng_log_record_type.sql
@@ -0,0 +1,23 @@
+DROP TYPE pws.cccng_syslog_record CASCADE ;
+
+CREATE TYPE pws.cccng_syslog_record AS 
+(
+	logId uuid,
+	syslogPriority int,
+	syslogEventTime timestamptz,
+	syslogHostIp inet,
+	syslogComponentString text,
+	syslogJobString text,
+	syslogIndexString text,
+	extmsgTimestamp timestamp,
+	extmsgMessage text,
+	extmsgLevel text,
+	extmsgSource text,
+	extmsgData text, 
+	extmsgThreadId text,
+	extmsgFiberId text,
+	extmsgProcessId text,
+	extmsgFile text,
+	extmsgLineNo text,
+	extmsgMethod text
+);
